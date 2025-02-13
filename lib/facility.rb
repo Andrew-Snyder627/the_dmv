fabf71d5a83967bdb85fa8a30275cd3214de1fde
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-13 17:33:43 -0500

    pass the first test

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,12 +1,19 @@
 class Facility
   attr_reader :name, :address, :phone, :services
 
-  def initialize(name, address, phone)
-    @name = name
-    @address = address
-    @phone = phone
+  def initialize(info) #since the input is a hash, I need to expect a hash. This is now expecting an info hash with name, address, phone
+    @name = info[:name]
+    @address = info[:address]
+    @phone = info[:phone]
     @services = []
   end
+
+  # def initialize(name:, address:, phone:) #failed attempt
+  #   @name = name
+  #   @address = address
+  #   @phone = phone
+  #   @services = []
+  # end
 
   def add_services(service)
     @services << service
