ad9cb8975d03c829ba1314cf4938ac0bc1ff7679
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-14 17:46:03 -0500

    Start the vehicle registration tests

--- lib/facility.rb
+++ lib/facility.rb
@@ -21,3 +21,21 @@ class Facility
 end
 
 #fixed the errors with dmv and passed test immediately
+
+#Iteration 2
+#Register a vehicle
+ #vehicles 25 years or older are antique, cost $25
+ #EVs cost $200 to register
+ #All other vehicles are $100 to register
+ #plate_type should be set to :regular, :antique, or :ev when registered
+
+#Administer written test
+ #must have a permit, and be 16 years old
+
+#Administer a road test
+ #Must have passed the written test
+ #registrants who qualify for the road test automatically get a license
+
+#Renew drivers license
+ #License can only be renewed if the registrant has already passed the road test and earned a license
+ 
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -24,3 +24,36 @@ RSpec.describe Facility do
     end
   end
 end
+
+RSpec.describe Facility do
+
+  describe '#Vehicle Registration' do #is the hashtag needed?
+    it 'starts with no registered vehicles' do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
+      facility_1.add_service('Vehicle Registration')
+
+      expect(cruz.registration_date).to be_nil
+      expect(facility_1.registered_vehicles).to eq([])
+      expect(facility_1.registered_vehicles).to eq([])
+      expect(facility_1.collected_fees).to eq(0)
+    end
+
+    it 'does not register vehicles if the service is not available' do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+
+      facility_2.register_vehicle(bolt)
+
+      expect(facility_2.registered_vehicles).to eq([])
+      expect(facility_2.collected_fees).to eq(0)
+    end
+  end
+end
