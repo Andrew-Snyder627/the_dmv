169ab1ec229efed85886d680285355a366478a46
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-15 10:17:09 -0500

    Add additional funtionality for the facility class

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,22 +1,48 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees
 
   def initialize(info) #since the input is a hash, I need to expect a hash. This is now expecting an info hash with name, address, phone
     @name = info[:name]
     @address = info[:address]
     @phone = info[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
-
-  # def initialize(name:, address:, phone:) #failed attempt
-  #   @name = name
-  #   @address = address
-  #   @phone = phone
-  #   @services = []
-  # end
 
   def add_service(service) #simple typo error
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    if @services.include?('Vehicle Registration') #checking if service is offered
+    else
+      return []
+    end
+
+    vehicle.registration_date = Date.today
+    plate_type = assign_plate_type(vehicle.year, vehicle.engine) #calling this method below
+    vehicle.plate_type = plate_type
+
+    fee = calculate_registration_fee(plate_type)
+    @collected_fees += fee
+    @registered_vehicles << vehicle
+
+    @registered_vehicles
+  end
+
+  def assign_plate_type(year, engine) #tried using a hash but failed epically
+    if engine == :ev
+      :ev
+    elsif year == 1969 #is there a better way to do this?
+      :antique
+    else
+      :regular
+    end
+  end
+
+  def calculate_registration_fee(plate_type) #takes plate type and assigns a fee value
+    fees = {:antique => 25, :ev => 200, :regular => 100}
   end
 end
 
@@ -38,4 +64,3 @@ end
 
 #Renew drivers license
  #License can only be renewed if the registrant has already passed the road test and earned a license
- 
